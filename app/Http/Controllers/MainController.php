8dd0122401b2b1b9e92d5bb2d8c93b1b2b838b3b
Angel H <anruhe.q@gmail.com>
2016-05-15 19:38:51 -0500

    Ordering added
    Order added in a DB way approach

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,12 +7,14 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class MainController extends BaseController {
 
+	//In this particular example we wil always use the json provied, so we can 
+	//initialize it on every request for being available on every function.
 	public function __construct() {
 		$this->raw_json = file_get_contents(storage_path() . '/prezis.json');
-		$this->json = json_decode($this->raw_json);
+		$this->json = json_decode($this->raw_json, true);
 	}
 
-    
+    //Skip flag is used for the load more feature, otherwise whe only return first 9 elements.
     public function prezis($skip = null) {
 
 		$prezis = [];
@@ -37,13 +39,37 @@ class MainController extends BaseController {
 		);
     }
 
+    //Sort by date DESC
+    public function prezisOrdered($skip = null) {
+
+		$prezis = new \Illuminate\Database\Eloquent\Collection();
+
+		//json createdAt value is transformed into a timestamp then added as a new property
+		foreach($this->json as $key => $prezi) {
+
+			$unformatted_date = date_create_from_format('F j, Y', $prezi['createdAt']);
+			$prezi['created_at'] = strtotime(date_format($unformatted_date, 'Y-m-d'));
+		    
+		    $prezis->add($prezi);	
+		}
+
+		$ordered = $prezis->sortByDesc('created_at')->slice($skip)->take(9)->values()->all();
+
+		return response()->json([
+				'status' => 'success',
+				'prezis' => json_encode($ordered)
+			]
+		);
+    }
+
+    //Search by title
     public function search($string) {
     	if(!empty($string)) {
 	    	$prezis = [];
 
 			foreach ($this->json as $key => $prezi) {
 
-				if (strlen(stristr($prezi->title, $string)) > 0) {
+				if (strlen(stristr($prezi['title'], $string)) > 0) {
 					$prezis[] = $prezi;
 				}
 			}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,12 +10,21 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$app->get('/', function(){
+	return 'success';
+});
 
 //Default route to be called
 $app->get('/prezis/', 'MainController@prezis');
 
 //This route will be used for pagination
 $app->get('/prezis/{skip}', 'MainController@prezis');
+
+//Ordered by date
+$app->get('/ordered/prezis', 'MainController@prezisOrdered');
+
+//Ordered by date and pagination
+$app->get('ordered/prezis/{skip}', 'MainController@prezisOrdered');
 
 //This route will search by title
 $app->get('/search/{string}', 'MainController@search');
